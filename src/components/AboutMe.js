aec5c323566fbb688bfb4a027b9f16ae12440f66
EuforiCoz <pecohades@gmail.com>
2023-07-29 15:54:42 +0200

    actualizar AboutMe

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -12,7 +12,7 @@ const AboutMe = () => {
                   <div class="col-md-7 my-auto">
                     <div class="card-body text-center">
                       <h1 class="card-title text-center ">Alejandro Javier Peco Romero</h1>
-                      <p className="text-center m-0 pb-3" style={{fontSize: "22px"}}>Desarrollador Frontend</p>
+                      <p className="text-center m-0 pb-3" style={{fontSize: "22px"}}>Desarrollador Web</p>
                       <p class="card-text text-center text-muted pb-4" style={{fontSize: "24px"}}>Ofrezco las mejores soluciones con las tecnologías más usadas en la actualidad con HTML, CSS y JavaScript. A día de hoy sigo obteniendo más conocimientos de programación y en reforzar lo aprendido</p>
                       <div className="d-flex flex-row justify-content-center gap-3">
                         <a className="btn text-white fs-5" href="#contactme" style={{backgroundColor: "#1190CB"}}>Contáctame</a>
